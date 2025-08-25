01dad503b8985144a28d5084401de2c114328779
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-08-25 14:11:45 +0000

    começado

---
+++ senha correta.py
@@ -0,0 +1,6 @@
+senha = int(input("Digite sua senha para entrar na sua conta bancaria. sua senha tem 6 digitos"))
+
+if senha == 102030:
+    print("senha correta")
+else:
+    print("senha incoreta")
