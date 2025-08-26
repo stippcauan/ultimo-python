0fb462cc2d2de645565c2f84f577b078b37eb083
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-08-26 13:32:11 +0000

    finalizado

--- senha correta.py
+++ senha correta.py
@@ -1,12 +1,7 @@
-import random
-
-def print(max_tentativas 3)
-
-    tentativas_feitas = 3
-
 senha = int(input("Digite sua senha para entrar na sua conta bancaria. sua senha tem 6 digitos  "))
 
 if senha == 102030:
     print("senha correta")
 
-elif print("senha incorreta"):
+else:
+    print("senha incorreta")
